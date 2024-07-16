554e17af5ce52aa2528ccfa2f118a417a70752ed
lukegjpotter <lukegjpotter@gmail.com>
2024-07-16 14:14:44 +0100

    Adding correct data to HTML Printer

--- src/main/java/com/lukegjpotter/tools/cyclingrouteutils/service/JsonToHtmlService.java
+++ src/main/java/com/lukegjpotter/tools/cyclingrouteutils/service/JsonToHtmlService.java
@@ -8,14 +8,14 @@ public class JsonToHtmlService {
 
     public String convertRoute(RouteUrlsRecord routeUrlsRecord) {
 
-        return """
+        return String.format("""
                 <html>
                     <head><title>Route URLs</title></head>
                     <body>
-                        <a href="https://www.strava.com/routes/123">Strava</a><br />
-                        <a href="https://www.veloviewer.com/routes/12">VeloViewer</a><br />
-                        <a href="https://mywindsock.com/route/123/#forecast=1694015100">MyWindSock</a>
+                        <a href="%s">Strava</a><br />
+                        <a href="%s">VeloViewer</a><br />
+                        <a href="%s">MyWindSock</a>
                     </body>
-                </html>""";
+                </html>""", routeUrlsRecord.sourceRoute(), routeUrlsRecord.veloViewerRoute(), routeUrlsRecord.myWindSockRoute());
     }
 }
